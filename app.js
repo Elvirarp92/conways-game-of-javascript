a89a38db53eac958551fb7afd2b08cd6f8bcf7ee
Elvira Ramírez <elvirarp92@gmail.com>
2020-08-06 18:30:06 +0200

    Render grid
    Create drawGrid() function to draw the grid used in the game. Every cell will actually be a shape so this might get scrapped from the final result but so far it is good reference to have.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let x
+let y
+
 const app = {
   name: "Conway's Game of Life",
   author: 'Elvira Ramírez Ponce',
@@ -24,13 +27,14 @@ const app = {
   fps: 1, //sic
   framesCounter: 0,
 
-  init(){
+  init() {
     this.ctx = this.canvasDom.getContext('2d')
     this.start()
   },
 
-  start(){
+  start() {
     this.setDimensions()
+    this.drawGrid()
   },
 
   setDimensions() {
@@ -38,5 +42,29 @@ const app = {
     this.appSize.width = this.tileSize * this.gridSize.columns
     this.canvasDom.height = this.appSize.height
     this.canvasDom.width = this.appSize.width
+  },
+
+  drawGrid() {
+    x = this.tileSize
+    y = this.tileSize
+    this.ctx.lineWidth = 0.2
+    this.ctx.globalAlpha = 1
+    while (x < this.appSize.width) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(x, 0)
+      this.ctx.lineTo(x, this.appSize.height)
+      this.ctx.stroke()
+      this.ctx.closePath()
+      x = x + this.tileSize
+    }
+
+    while (y < this.appSize.height) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(0, y)
+      this.ctx.lineTo(this.appSize.width, y)
+      this.ctx.stroke()
+      this.ctx.closePath()
+      y = y + this.tileSize
+    }
   },
 }
