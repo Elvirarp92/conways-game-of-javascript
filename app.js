11af750b8d7b900d2946b72abf94ab4ae7504c35
Elvira Ramírez <elvirarp92@gmail.com>
2020-08-06 17:15:09 +0200

    Set dimensions programatically
    Create main function of the app object, where the game loop will run, and a function to set the canvas dimensions depending on tile size and number of wanted tiles

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const app = {
   name: "Conway's Game of Life",
-  author: "Elvira Ramírez Ponce",
-  version: "1.0",
+  author: 'Elvira Ramírez Ponce',
+  version: '1.0',
   license: undefined,
 
-  canvasDom: document.getElementById("game-board"),
+  canvasDom: document.getElementById('game-board'),
   ctx: undefined,
 
   tileSize: 10,
 
   appSize: {
-    // tile size * number of tiles on axis
-    height: 10 * 60,
-    width: 10 * 80
+    // size, in pixels, of the canvas
+    height: undefined,
+    width: undefined,
+  },
+
+  gridSize: {
+    // number of tiles in every axis
+    rows: 60,
+    columns: 80,
   },
 
   fps: 1, //sic
   framesCounter: 0,
-  
 
+  init(){
+    this.ctx = this.canvasDom.getContext('2d')
+    this.start()
+  },
+
+  start(){
+    this.setDimensions()
+  },
+
+  setDimensions() {
+    this.appSize.height = this.tileSize * this.gridSize.rows
+    this.appSize.width = this.tileSize * this.gridSize.columns
+    this.canvasDom.height = this.appSize.height
+    this.canvasDom.width = this.appSize.width
+  },
 }
